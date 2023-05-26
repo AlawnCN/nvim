e8c226200e96b150d116f75b9343e5d8289e97b5
Alawn <alawn.cn@gmail.com>
2023-05-26 10:15:50 +0800

    add plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,123 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+
+opt.autowrite = true -- Enable auto write
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = true -- Use spaces instead of tabs
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true -- Ignore case
+opt.inccommand = "nosplit" -- preview incremental substitute
+opt.laststatus = 0
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.relativenumber = true -- Relative line numbers
+opt.scrolloff = 4 -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 4 -- Size of an indent
+opt.shortmess:append({ W = true, I = true, c = true })
+opt.showmode = false -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8 -- Columns of context
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.tabstop = 4 -- Number of spaces tabs count for
+opt.softtabstop = 4
+opt.autoindent = true
+opt.termguicolors = true -- True color support
+opt.timeoutlen = 300
+opt.undofile = true
+opt.undolevels = 10000
+opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.winminwidth = 5 -- Minimum window width
+opt.wrap = false -- Disable line wrap
+opt.encoding = 'UTF-8'
+opt.showtabline = 1
+
+vim.opt.background = "dark" -- set this to dark or light
+
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+  opt.splitkeep = "screen"
+  opt.shortmess:append({ C = true })
+end
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
+vim.o.guifont = "Inconsolata for Powerline:h13.5" -- text below applies for VimScript
+
+
+-- Neovide Setting
+if vim.g.neovide then
+    -- vim.o.guifont = "Source Code Pro:h14" -- text below applies for VimScript
+    -- vim.o.guifont = "Inconsolata for Powerline:h12.5" -- text below applies for VimScript
+    -- vim.opt.linespace = 0
+    -- vim.g.neovide_scale_factor = 1.0
+    vim.g.neovide_padding_top = 20
+    vim.g.neovide_padding_bottom = 20 
+    vim.g.neovide_padding_right = 20 
+    vim.g.neovide_padding_left = 20 
+
+    -- Background color
+    local alpha = function()
+      return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+    end
+    -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+    vim.g.neovide_transparency = 0.0
+    vim.g.transparency = 0.8
+    vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+    -- Transparency
+    vim.g.neovide_transparency = 0.8
+
+    vim.g.neovide_scroll_animation_length = 0.3
+
+    vim.g.neovide_hide_mouse_when_typing = false
+    vim.g.neovide_underline_automatic_scaling = false
+    vim.g.neovide_refresh_rate = 60
+    vim.g.neovide_refresh_rate_idle = 5
+    vim.g.neovide_no_idle = true
+    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_fullscreen = false
+    vim.g.neovide_remember_window_size = true
+    vim.g.neovide_profiler = false
+    -- vim.g.neovide_input_use_logo = false
+    vim.g.neovide_input_macos_alt_is_meta = true
+    vim.g.neovide_touch_deadzone = 6.0
+    vim.g.neovide_touch_drag_timeout = 0.17
+    vim.g.neovide_cursor_animation_length = 0.13
+    vim.g.neovide_cursor_trail_size = 0.8
+    vim.g.neovide_cursor_antialiasing = true
+    vim.g.neovide_cursor_animate_in_insert_mode = true
+    vim.g.neovide_cursor_animate_command_line = true
+    vim.g.neovide_cursor_unfocused_outline_width = 0.125
+    -- railgun, torpedo, pixiedust,sonicboom, ripple, wireframe
+    vim.g.neovide_cursor_vfx_mode = "ripple" -- torpedo, pixiedust,sonicboom, ripple, wireframe
+    vim.g.neovide_cursor_vfx_opacity = 200.0
+    -- vim.g.neovide_cursor_vfx_opacity = 0.0
+    vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
+    -- vim.g.neovide_cursor_vfx_particle_lifetime = 0.0
+    vim.g.neovide_cursor_vfx_particle_density = 7.0
+    vim.g.neovide_cursor_vfx_particle_speed = 10.0
+    vim.g.neovide_cursor_vfx_particle_phase = 1.5
+    vim.g.neovide_cursor_vfx_particle_curl = 1.0
+end
+
